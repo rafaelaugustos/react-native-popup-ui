5049cbd13b31fe13c4c26bdf19205c01a4526f1a
Julio Augusto <julioams@icloud.com>
2020-10-24 15:28:55 -0300

    change example files

--- examples/Root.js
+++ examples/Root.js
@@ -1,22 +1,32 @@
-import React, {Component} from 'react';
-import {View, ViewPropTypes} from 'react-native';
+import React, { Component } from 'react';
+import { View, ViewPropTypes } from 'react-native';
 import PropTypes from 'prop-types';
 
+import Popup from './Popup';
 import Toast from './Toast';
 
 class Root extends Component {
   render() {
     return (
-      <View ref={c => (this._root = c)} style={{flex: 1}} {...this.props}>
+      <View
+        ref={c => (this._root = c)}
+        style={{ flex: 1 }}
+        {...this.props}
+      >
         {this.props.children}
+        <Popup
+          ref={c => {
+            if (c) Popup.popupInstance = c
+          }}
+        />
 
         <Toast
           ref={c => {
-            if (c) Toast.toastInstance = c;
+            if (c) Toast.toastInstance = c
           }}
         />
       </View>
-    );
+    )
   }
 }
 
@@ -25,8 +35,8 @@ Root.propTypes = {
   style: PropTypes.oneOfType([
     PropTypes.object,
     PropTypes.number,
-    PropTypes.array,
-  ]),
-};
+    PropTypes.array
+  ])
+}
 
-export default Root;
+export default Root
