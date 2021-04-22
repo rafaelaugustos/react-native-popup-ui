76fe6659c75cf76f5a8d22ffe4749220ca565252
Julio Augusto <julioams@icloud.com>
2021-04-21 21:56:45 -0300

    refactor: change file extension

--- example/index.ts
+++ example/index.ts
@@ -1,5 +1,6 @@
 import {AppRegistry} from 'react-native';
-import App from './src/basic/popup';
+import App from './src/popup';
+// @ts-ignore
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
